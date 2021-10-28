1cd23619f5b6ab87de9889e0a525e3fb2278cf5e
EriWinckler <efwinckler@gmail.com>
2021-10-28 10:49:22 -0400

    first commit

---
+++ exercise-10-28-21/src/main/java/com/example/exercise102821/Exercise102821Application.java
@@ -0,0 +1,13 @@
+package com.example.exercise102821;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise102821Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise102821Application.class, args);
+	}
+
+}
---
+++ exercise-10-28-21/src/test/java/com/example/exercise102821/Exercise102821ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise102821;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise102821ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
